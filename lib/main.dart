df4b7005d259be4f4f89582f389a9f28285cd7fc
LENOVO <trinetrakumar7250@gmail.com>
2024-12-19 14:47:27 +0530

    MyCommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get.dart';
 import 'myprofilepage.dart';
 
 void main()
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyProfilePage(),
     );
